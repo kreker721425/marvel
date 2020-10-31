c4cbffe9cfc331ea6f6e5ddfaefc83b001a23dca
kreker721425 <spiridonovdaniil777@gmail.com>
2020-11-01 00:07:51 +0300

    update mappers

--- src/main/java/com/github/kreker721425/marvel/mapper/CharacterMapper.java
+++ src/main/java/com/github/kreker721425/marvel/mapper/CharacterMapper.java
@@ -6,7 +6,7 @@ import org.mapstruct.Mapper;
 
 import java.util.Collection;
 
-@Mapper
+@Mapper(componentModel="spring")
 public interface CharacterMapper {
 
     CharacterDto toCharacterDto(CharacterEntity characterEntity);
