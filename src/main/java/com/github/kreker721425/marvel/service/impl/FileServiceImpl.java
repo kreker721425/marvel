b58f2acaa4248eaa5c813bbfe7c5d878145202cf
kreker721425 <spiridonovdaniil777@gmail.com>
2020-11-01 01:14:26 +0300

    updated FileServiceImpl

--- src/main/java/com/github/kreker721425/marvel/service/impl/FileServiceImpl.java
+++ src/main/java/com/github/kreker721425/marvel/service/impl/FileServiceImpl.java
@@ -7,6 +7,7 @@ import com.github.kreker721425.marvel.exception.FileStorageException;
 import com.github.kreker721425.marvel.service.FileService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -15,29 +16,33 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.UUID;
 
+@Service
 public class FileServiceImpl implements FileService {
 
     @Value("${upload.path}")
     private String uploadPath;
 
-    public UploadResponseDto uploadFile(MultipartFile file) {
-        if (file != null && !file.isEmpty()) {
-            File uploadFile = new File(uploadPath);
-            if (!uploadFile.exists()) {
-                uploadFile.mkdir();
-            }
-            String fileName = UUID.randomUUID() + file.getOriginalFilename();
 
-            try {
-                file.transferTo(Paths.get(uploadPath).resolve(fileName));
-                return new UploadResponseDto(fileName);
-            } catch (IOException e) {
-                throw new FileStorageException();
-            }
-        } else
+    @Override
+    public UploadResponseDto uploadFile(MultipartFile file) {
+        if (file == null || file.isEmpty()) {
             throw new FileIsEmptyException();
+        }
+        File uploadFile = new File(uploadPath);
+        if (!uploadFile.exists()) {
+            uploadFile.mkdir();
+        }
+        String fileName = UUID.randomUUID() + file.getOriginalFilename();
+
+        try {
+            file.transferTo(Paths.get(uploadPath).resolve(fileName));
+            return new UploadResponseDto(fileName);
+        } catch (IOException e) {
+            throw new FileStorageException();
+        }
     }
 
+    @Override
     public UploadInfo get(String id) {
         try {
             return new UploadInfo(
@@ -48,6 +53,4 @@ public class FileServiceImpl implements FileService {
             throw new FileStorageException(e);
         }
     }
-
-
 }
